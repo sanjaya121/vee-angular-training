d8902e0bfbe5cfc29dd311ecd5a6f532f0fb5b70
Sanjaya Chaudhary <sanjaya121@dgmail.com>
2022-06-16 12:25:31 -0500

    some coding challenge

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MaxLengthValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  arr = [1, 2, 3, 4, 56, 77];
+  array = ["Mike", "Matt", "Nancy", "Adam", "Jenny", "Nancy", "Carl"];
+  height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+  // height = [1, 2]
   constructor() { }
 
   ngOnInit(): void {
+    // this.findMissingNUmber(this.arr);
+    // this.uniqueArray(this.array);
+    this.maxArea(this.height);
   }
+  //finding Missing Number
+  // findMissingNUmber = (arr) => {
+  //   var missing = []
+  //   let minNumber = Math.min(...arr);
+  //   let maxNumber = Math.max(...arr);
+  //   console.log(minNumber, maxNumber)
+  //   for (var i = minNumber; i <= maxNumber; i++) {
+  //     if (arr.indexOf(i) < 0) {
+  //       missing.push(i)
+  //     }
+  //   }
+  //   console.log(missing);
+  // }
 
+
+
+  // remove duplicate
+  // uniqueArray = (arr) => {
+  //   let unique = arr.filter((array, index) => {
+  //     return arr.indexOf(array) == index;
+
+  //   })
+  //   console.log(unique);
+  // }
+  /*
+    You are given an integer array height of length n. 
+    There are n vertical lines drawn such that the 
+    two endpoints of the ith line are (i, 0) and (i, height[i]).
+  
+  Find two lines that together with the x-axis form 
+  a container, such that the container contains the most water.
+  
+  Return the maximum amount of water a container can store.
+  
+  Notice that you may not slant the container.
+  
+  */
+
+  maxArea = (height) => {
+    let area = 0;
+    for (let i = 0; i < height.length; i++) {
+      for (let j = 0; j < height.length; j++) {
+        area = Math.max(area, (j - i) * Math.min(height[i], height[j]))
+      }
+    }
+    //  i = 0, j = height.length - 1;
+    // while (i < j) {
+    //   const temp = (j - 1) * Math.min(height[i], height[j]);
+    //   area = Math.max(area, temp);
+    //   if (height[i] > height[j]) {
+    //     j -= 1;
+    //   }
+    //   else {
+    //     i += 1;
+    //   }
+    // }
+
+    console.log(area);
+    console.log(Math.min(4, 5))
+  }
 }
